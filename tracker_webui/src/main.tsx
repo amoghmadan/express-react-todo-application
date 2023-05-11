298d68b639bd54971b80abdda269819a8650532d
Amogh Madan <amoghmadaan@gmail.com>
2023-05-11 18:38:17 +0530

    Wrap redux around application.

--- tracker_webui/src/main.tsx
+++ tracker_webui/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from "./App.tsx";
+import store from "./store";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
