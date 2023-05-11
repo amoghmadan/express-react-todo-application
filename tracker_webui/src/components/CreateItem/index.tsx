9c96647cd5a46ac07efd537047c0f4f5bb3f4a79
Amogh Madan <amoghmadaan@gmail.com>
2023-05-11 14:54:18 +0530

    Fix CSS for input.

--- tracker_webui/src/components/CreateItem/index.tsx
+++ tracker_webui/src/components/CreateItem/index.tsx
@@ -1,13 +1,16 @@
 import { AxiosResponse } from "axios";
-import { MutableRefObject, useRef } from "react";
+import { FormEvent, MutableRefObject, useRef } from "react";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 
 import { TaskAPI } from "../../constants";
 import { axios } from "../../services";
 
 export default function CreateItem(): JSX.Element {
+  const navigate: NavigateFunction = useNavigate();
   const textRef: MutableRefObject<HTMLInputElement> = useRef(null!);
 
-  const onSubmit = async (): Promise<void> => {
+  const onSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
     const response: AxiosResponse = await axios.post(TaskAPI.ITEM, {
       text: textRef.current.value,
     });
@@ -18,25 +21,28 @@ export default function CreateItem(): JSX.Element {
         alert("Something went wrong!");
       }
     }
+    navigate(0);
   };
 
   return (
     <div className="justify-center mt-4 flex">
       <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          id="text"
-          placeholder="Text"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          ref={textRef}
-          required={true}
-        />
-        <button
-          type="submit"
-          className="middle none center rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-        >
-          Add
-        </button>
+        <div className="flex items-center py-2">
+          <input
+            type="text"
+            id="text"
+            placeholder="Text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            ref={textRef}
+            required={true}
+          />
+          <button
+            type="submit"
+            className="middle none center rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          >
+            Add
+          </button>
+        </div>
       </form>
     </div>
   );
